ff318e3903c2b32034d32428452366f67d312197
Peter (Rocky) Villano <github@saej.in>
2026-03-22 22:43:51 -0700

    better readme

---
+++ README.md
@@ -0,0 +1,4 @@
+Registration of languages does not work in Vite 8
+
+## Screenshot
+![img_1.png](screenshot.png)
--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,17 @@ import {MyEditor} from "./MyEditor.jsx";
 import {languageMap} from "prism-react-editor";
 import {languages} from "prism-react-editor/prism";
 
-console.log({languageMap, languages})
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <h2>languages entries</h2>
+    <ul>
+      {Object.keys(languages).map(language => <li key={language}>{language}</li>)}
+    </ul>
+    <h2>languageMap entries</h2>
+    <ul>
+      {Object.keys(languageMap).map(language => <li key={language}>{language}</li>)}
+    </ul>
+      <h2>Editor missing highlighting</h2>
     <MyEditor/>
   </StrictMode>,
 )
